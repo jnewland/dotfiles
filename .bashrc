c4dc19727c9b43e20212a1aead40a2656fe342b4
Jesse Newland <jesse@jnewland.com>
2022-06-22 17:15:56 +0000

    tailscale

--- .bashrc
+++ .bashrc
@@ -1,5 +1,6 @@
-echo "dotfiles greetings" 
-echo "enabling real-time bash history..."
+# shellcheck shell=bash
+echo "shell configured with https://github.com/jnewland/dotfiles"
 
 shopt -s histappend
 export PROMPT_COMMAND="history -a;$PROMPT_COMMAND"
+export PATH="$HOME/bin:$PATH"
--- setup.sh
+++ setup.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
-set -ex
+set -e
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+echo "running setup.sh from https://github.com/jnewland/dotfiles"
 
 if ! grep -q $SCRIPTPATH/.bashrc ~/.bashrc; then
   echo ". $SCRIPTPATH/.bashrc" >> ~/.bashrc
 fi
+
+mkdir -p ~/bin
+
+function extract_file_from_image() {
+  container_id=$(docker create "$1")
+  docker cp "$container_id:$2" "$3"
+  docker rm "$container_id" > /dev/null
+}
+
+echo "installing tailscale..."
+extract_file_from_image ghcr.io/tailscale/tailscale:stable /usr/local/bin/tailscaled ~/bin/tailscaled
+extract_file_from_image ghcr.io/tailscale/tailscale:stable /usr/local/bin/tailscale ~/bin/tailscale
+sudo mv ~/bin/tailscale ~/bin/tailscaled /usr/sbin/
+
+sudo mkdir -p /var/run/tailscale /var/cache/tailscale /var/lib/tailscale
+sudo cp $SCRIPTPATH/tailscaled /etc/init.d/
+sudo chmod a+x /etc/init.d/tailscaled
+
+sudo cp $SCRIPTPATH/start-tailscale ~/bin
+
+~/bin/start-tailscale
